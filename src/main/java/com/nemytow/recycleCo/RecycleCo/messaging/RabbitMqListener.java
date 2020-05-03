5fb4ee7b6a7dee70ac7fe6aea475b293b20391a4
ekbhard <ekbhard@gmail.com>
2020-05-03 17:20:33 +0500

    fix rabbit mq

--- src/main/java/com/nemytow/recycleCo/RecycleCo/messaging/RabbitMqListener.java
+++ src/main/java/com/nemytow/recycleCo/RecycleCo/messaging/RabbitMqListener.java
@@ -2,25 +2,33 @@ package com.nemytow.recycleCo.RecycleCo.messaging;
 
 import com.google.gson.Gson;
 import com.nemytow.recycleCo.RecycleCo.api.messaging.MessagingApi;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
+@Log4j2
 @Component
-public class RabbitMqListener {
+public class RabbitMqListener implements MessageListener {
 
     @Autowired
     MessagingApi api;
 
+    @SneakyThrows
+    @Override
     @RabbitListener(queues="${rabbitmq.pythonQueue}")
-    public void listen(byte[] message) {
-        String msg = new String(message);
-        TrashMessage not = new Gson().fromJson(msg, TrashMessage.class);
-        System.out.println("Received a new message..." + not.toString());
+    public void onMessage(Message message) {
+        String json = new String(message.getBody());
+        TrashMessage not = new Gson().fromJson(json, TrashMessage.class);
+        log.info("Received a new message..." + json);
         Long idCreated = api.saveTrashFromTheQueue(not);
         if (idCreated==null){
-            System.out.println("Message does not saved");
+            log.error("Message does not saved");
+            return;
         }
+        log.info("Trash saved : " + idCreated);
     }
 }
